244c569624eacaef73a2698c428c8b09ae4767ae
Zambrinn <zambrinfilho@gmail.com>
2025-10-27 17:40:26 -0300

    feat: add CRUD operations for study topics in StudyTopicController and StudyTopicService

--- src/main/kotlin/com/github/Zambrinn/KotlinAPI/controller/StudyTopicController.kt
+++ src/main/kotlin/com/github/Zambrinn/KotlinAPI/controller/StudyTopicController.kt
@@ -5,11 +5,17 @@ import com.github.Zambrinn.KotlinAPI.dto.StudyTopicResponse
 import com.github.Zambrinn.KotlinAPI.model.StudyTopic
 import com.github.Zambrinn.KotlinAPI.service.StudyTopicService
 import jakarta.validation.Valid
+import org.apache.coyote.Response
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import java.net.URI
 
@@ -30,4 +36,29 @@ class StudyTopicController (
     fun listAllTopics(): List<StudyTopicResponse> {
         return service.getAllTopics();
     }
-}
+
+    @GetMapping("/{id}")
+    fun getTopicById(@PathVariable id: Long): ResponseEntity<StudyTopicResponse> {
+        val foundTopic = service.getTopicById(id)
+
+        return foundTopic?.let { topic -> ResponseEntity.ok(topic) }
+            ?: ResponseEntity.notFound().build()
+    }
+
+    @PutMapping("/{id}")
+    fun updateTopic(@PathVariable id: Long, @Valid @RequestBody request: StudyTopicRequest): ResponseEntity<*>? {
+        return service.updateTopic(id, request)?.let {
+            updatedTopic ->
+            ResponseEntity.ok(updatedTopic) }
+                ?: ResponseEntity.notFound().build<Void>()
+        }
+
+    @DeleteMapping("/{id}")
+    fun deleteTopic(@PathVariable id: Long): ResponseEntity<*> {
+            return if (service.deleteTopic(id)) {
+                ResponseEntity.noContent().build<Void>()
+            } else {
+                ResponseEntity.noContent().build<Void>()
+            }
+        }
+    }
--- src/main/kotlin/com/github/Zambrinn/KotlinAPI/service/StudyTopicService.kt
+++ src/main/kotlin/com/github/Zambrinn/KotlinAPI/service/StudyTopicService.kt
@@ -30,19 +30,30 @@ class StudyTopicService(
 
     fun getTopicById(id: Long): StudyTopicResponse? {
         val entityFound = repository.findByIdOrNull(id);
-
         return entityFound?.toResponse();
     }
 
-    fun deleteTopicById(id: Long): StudyTopicResponse? {
-        val entityToDelete = repository.deleteById(id)
-
-        return entityToDelete?.toResponse();
+    fun updateTopic(id: Long, request: StudyTopicRequest): StudyTopicResponse? {
+        return repository.findByIdOrNull(id)?.let { existingTopic ->
+            val updatedTopic = existingTopic.copy(
+                name = request.name,
+                category = request.category
+            )
+            repository.save(updatedTopic).toResponse()
+        }
     }
+    fun deleteTopic(id: Long): Boolean {
+        if (repository.existsById(id)) {
+            repository.deleteById(id)
+            return true
+        }
+        return false
+    }
+
 
     private fun StudyTopic.toResponse(): StudyTopicResponse {
         return StudyTopicResponse(
-            id = this.id!!,
+            id = this.id!!, // non nullable object
             name = this.name,
             category = this.category,
             status = this.status,
